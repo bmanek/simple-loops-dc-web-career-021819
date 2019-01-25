af8d4f73b47521d1838fad1b4a1c786d8612cbf8
Ben Manekin <ben.manekin@gmail.com>
2019-01-25 01:52:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -23,8 +23,7 @@ def times_iterator(number_of_times)
   number_of_times.times do
     puts phrase
 end
-  
-end
+
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword

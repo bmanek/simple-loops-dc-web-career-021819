20543e4f2f5acc909e97784dcf61607fff8862f5
Ben Manekin <ben.manekin@gmail.com>
2019-01-25 01:50:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -19,7 +19,6 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
